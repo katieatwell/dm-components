731bc82a03edad7d1de3657ff10585bbb84f7513
Katie Atwell <katieatwell@outlook.com>
2019-04-19 10:10:45 -0400

    edits

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,15 +2,22 @@ const path = require('path');
 
 module.exports = {
   module: {
-   rules: [
+    rules: [
       {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../src')
-     },
-     {  test: /\.css$/,
-        loader: 'style-loader!css-loader',
-        include: __dirname
-     },]
-  }
+        loaders: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+          require.resolve('sass-loader')
+        ],
+      },
+    ],
+  },
 }
